003c08b40c06526fa549403ef659655e380d51ef
Bijal Patel <35340849+bijalpatel22@users.noreply.github.com>
2022-02-19 23:18:25 -0800

    Updated

--- Client/Lab4Api.java
+++ Client/Lab4Api.java
@@ -16,14 +16,14 @@ public class Lab4Api {
         public int startTime;
         public int skierIDStart;
         public double runTimeIdx;
-        public double numOfThread;
+        public double percentageOfThread;
         String phase;
 
-        public Info(int startTime, int skierIDStart, double runTimeIdx, double numOfThread, String phase) {
+        public Info(int startTime, int skierIDStart, double runTimeIdx, double percentageOfThread, String phase) {
             this.startTime = startTime;
             this.skierIDStart = skierIDStart;
             this.runTimeIdx = runTimeIdx;
-            this.numOfThread = numOfThread;
+            this.percentageOfThread = percentageOfThread;
             this.phase = phase;
         }
     }
@@ -35,27 +35,36 @@ public class Lab4Api {
     final static private int NUMLIFTs = 40; //range from 5 to 60, by default 40
     static private Random rand = new Random();
     static private Map<String, Integer> statusCodeMap = new HashMap<>();
+    static File file;
+    static FileWriter writer;
+
+
+
 
 
     synchronized public  void incCall(SkiersApi apiInstance, ApiClient client, Info info) {
-        int skierPerThread = NUMSKIERS / info.numOfThread;
+        int numOfThread = (int)(NUMTHREADS * info.percentageOfThread);
+        int skierPerThread = NUMSKIERS / numOfThread;
         int currSkierStart = info.skierIDStart;
         info.skierIDStart += skierPerThread;
 
-        for (int i = 0; i < info.runTimeIdx * NUMRUNS; i++) {
-            for (int j = currSkierStart; j < currSkierStart + skierPerThread; j++) {
+        for (int i = 0; i < info.runTimeIdx * NUMRUNS; i++) { // num run
+            for (int j = currSkierStart; j < currSkierStart + skierPerThread; j++) { //skier in range
                 Integer resortID = 56; // Integer | ID of the resort
                 String seasonID = "56"; // String | ID of the season
                 String dayID = "56"; // String | ID of the day
                 try {
+                    long threadStart = System.nanoTime();
                     ApiResponse res = apiInstance.getSkierDayVerticalWithHttpInfo(resortID, seasonID, dayID, j);
+                    long threadEnd = System.nanoTime();
                     String key = info.phase + " " + res.getStatusCode();
                     statusCodeMap.put(key, statusCodeMap.getOrDefault(key, 0) + 1);
 
                     //System.out.println(res.getStatusCode());
                     //Integer verticalResult = apiInstance.getSkierDayVertical(resortID, seasonID, dayID, skierID);
                     //System.out.println(verticalResult);
-                } catch (ApiException e) {
+                    writer.write(String.valueOf((threadEnd - threadStart) + "\n"));
+                } catch (ApiException | IOException e) {
                     System.err.println("Exception when calling SkiersApi#getSkierDayVertical");
                     e.printStackTrace();
                 }
@@ -65,27 +74,35 @@ public class Lab4Api {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         long start = System.currentTimeMillis();
-        File file = new File(NUMTHREADS + " Threads.txt");
-        file.createNewFile();
+
 
         //phase 1
         System.out.println("Phase One Started");
         Lab4Api lab4Api = new Lab4Api();
-        FileWriter writer = new FileWriter(file);
 
-        int numOfThreadOne = (int) (NUMTHREADS * 0.25);
-        int numOfThreadTwo = (int) (NUMTHREADS * 1d);
-        int numOfThreadThree = (int) (NUMTHREADS * 0.1);
+            try {
+                file = new File("Multi-Thread-Performance.txt");
+                file.createNewFile();
+                writer = new FileWriter(file);
+            } catch (final IOException e) {
+                throw new ExceptionInInitializerError(e.getMessage());
+            }
 
-        Info infoPhaseOne = new Info(0, 0, 0.2, numOfThreadOne, "PhaseOne");
-        Info infoPhaseTwo = new Info(91, 0, 0.6, numOfThreadTwo, "PhaseTwo");
-        Info infoPhaseThree = new Info(361, 0, 0.1, numOfThreadThree, "PhaseThree");
+
+        Info infoPhaseOne = new Info(0, 0, 0.2, 0.25, "PhaseOne");
+        Info infoPhaseTwo = new Info(91, 0, 0.6, 1d, "PhaseTwo");
+        Info infoPhaseThree = new Info(361, 0, 0.1, 0.1, "PhaseThree");
+
+        int numOfThreadOne = (int) (NUMTHREADS * infoPhaseOne.percentageOfThread);
+        int numOfThreadTwo = (int) (NUMTHREADS * infoPhaseTwo.percentageOfThread);
+        int numOfThreadThree = (int) (NUMTHREADS * infoPhaseThree.percentageOfThread);;
 
         CountDownLatch completedOne = new CountDownLatch((int) Math.ceil(numOfThreadOne * 0.2));
         CountDownLatch completedTwo = new CountDownLatch((int) Math.ceil(numOfThreadTwo * 0.2));
         CountDownLatch completedTotal = new CountDownLatch(numOfThreadThree + numOfThreadTwo + numOfThreadOne);
 
         //Phase 1
+        System.out.println("Phase 1 time is " + System.currentTimeMillis());
 
         for (int i = 0; i < numOfThreadOne; i++) {
             long threadStart = System.currentTimeMillis();
@@ -109,6 +126,7 @@ public class Lab4Api {
 
         //Phase 2
         System.out.println("Phase Two Started");
+        System.out.println("Phase 2 time is " + System.currentTimeMillis());
 
         for (int i = 0; i < numOfThreadTwo; i++) {
             long threadStart = System.currentTimeMillis();
@@ -126,11 +144,12 @@ public class Lab4Api {
             //writer.write(String.valueOf((threadEnd - threadStart) + "\n"));
         }
 
-        completedOne.await();
+        completedTwo.await();
 
 
         //Phase 3
         System.out.println("Phase Three Started");
+        System.out.println("Phase 3 time is " + System.currentTimeMillis());
 
         for (int i = 0; i < numOfThreadThree; i++) {
             long threadStart = System.currentTimeMillis();
